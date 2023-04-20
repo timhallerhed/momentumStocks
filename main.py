ebac5cd5d5db45ab2b601f183464f8addffa5e21
timhallerhed <tim.hallerhed@hotmail.com>
2023-04-20 09:55:52 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+# utgår helt och hållet från (men skapar virtual env först https://www.freecodecamp.org/news/how-to-setup-virtual-environments-in-python/ ) https://www.youtube.com/watch?v=dam0GPOAvVI
+
+from website import create_app
+
+app = create_app()
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
